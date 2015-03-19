eaa124412070a9251d1dd42c551abc3001ec454d
MewFree <damien.gonot@gmail.com>
2015-03-18 23:17:08 -0400

    new feature: being accessible on all flows

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ The bot searches the first search result on Giphy.com and display the result in 
 
 Example: "!gifbot new york" should display a New York GIF.
 
-Requires a conf.json file with a flowdock API key ("fdkey"), a flow ID ("flowid") and a Giphy API key ("giphykey")
+Requires a conf.json file with a flowdock API key ("fdkey") and a Giphy API key ("giphykey")
--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 var conf = require('./conf.json');
 var request = require('request');
 var Session = require('flowdock').Session;
+var session = new Session(conf.fdkey);
 
 var CODE = '!gifbot';
-var s = new Session(conf.fdkey);
-var flowid = conf.flowid;
-var stream = s.stream(flowid);
 
-stream.on('message', function(message) {
-  if (message.event == 'message') {
-    var query = message.content;
-    query = query.split(' ');
-    if (query[0] == CODE) {
-      query.shift();
-      query = query.toString().replace(/,/g, '+');
-      request('http://api.giphy.com/v1/gifs/random?api_key='+conf.giphykey+'&tag='+query, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var gif = JSON.parse(body).data.image_original_url;
-          s.comment(flowid, message.id, gif);
-        }
-      });
+session.flows(function(err, flows) {
+  var anotherStream, flowIds;
+  flowIds = flows.map(function(f) {
+    return f.id;
+  });
+  anotherStream = session.stream(flowIds);
+  return anotherStream.on('message', function(msg) {
+    if (message.event == 'message') {
+      var query = message.content;
+      query = query.split(' ');
+      if (query[0] == CODE) {
+        query.shift();
+        query = query.toString().replace(/,/g, '+');
+        request('http://api.giphy.com/v1/gifs/random?api_key='+conf.giphykey+'&tag='+query, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var gif = JSON.parse(body).data.image_original_url;
+            session.comment(flowid, message.id, gif);
+          }
+        });
+      }
     }
-  }
+  });
 });
